24647baa481f598b59683d3a313db4ae61182da8
NoumanHashimi <44499559+NoumanHashimi@users.noreply.github.com>
2022-11-18 14:52:44 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,5 @@
-string = "Hi World"
-all_words = string.split()
-first_word= all_words[0]
-if first_word != "Hello":
-    raise Exception("Hello Not Found!")
-else:
-    print(string)
+from logic import my_function
+
+my_function("Hello World")
+
+
