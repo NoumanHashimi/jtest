0057acf879bcd1bd92452530967378aa553f427c
NoumanHashimi <44499559+NoumanHashimi@users.noreply.github.com>
2022-11-18 14:56:17 +0530

    Update logic.py

--- logic.py
+++ logic.py
@@ -1,6 +1,7 @@
-all_words = string.split()
-first_word= all_words[0]
-if first_word != "Hello":
-    raise Exception("Hello Not Found!")
-else:
-    print(string)
+def my_func(string):
+    all_words = string.split()
+    first_word= all_words[0]
+    if first_word != "Hello":
+        raise Exception("Hello Not Found!")
+    else:
+        print(string)
